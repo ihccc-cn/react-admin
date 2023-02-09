df031a231ba7eb90988b2880141c9122816125c3
liucdev <1058807007@qq.com>
2023-02-09 18:29:12 +0800

    perf: 调整按钮位置

--- src/pages/form/form-render/editor/components/tab-fields/index.jsx
+++ src/pages/form/form-render/editor/components/tab-fields/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { ReactSortable } from "react-sortablejs";
-import { Tabs, Collapse, Button } from "antd";
+import { Tabs, Collapse, Button, Empty } from "antd";
 import Icon from "@/common/components/icon";
-import { uuid, stopPropagationEvent } from "../../../utils";
+import { uuid } from "../../../utils";
 import CellField from "../cell-field";
 import IconTip from "../icon-tip";
 
@@ -20,23 +20,23 @@ function ComponentPanel({ groupName, nodes, rowKey, onItem, onAdd }) {
   return (
     <Collapse accordion bordered={false} defaultActiveKey={[source[0][rowKey]]}>
       {source.map((group, index) => (
-        <Collapse.Panel
-          header={group.group}
-          {...(group.addAble ? { extra: <Button type="link" size="small" icon={<Icon type="icon-add" />} onClick={stopPropagationEvent(onAdd)} /> } : {})}
-          key={group[rowKey]}
-        >
-          <ReactSortable
-            animation={150}
-            list={group.children}
-            setList={list => setList(index, list)}
-            group={{ name: groupName, pull: "clone", put: false }}
-            clone={item => ({ ...item, [rowKey]: uuid() })}
-            sort={false}
-          >
-            {group.children.map(item => (
-              <CellField title={item.title} type={item.type} icon={item.icon} onClick={() => onItem({ ...item, [rowKey]: uuid() })} key={item[rowKey]} />
-            ))}
-          </ReactSortable>
+        <Collapse.Panel header={group.group} key={group[rowKey]}>
+          {group.children.length === 0 ? (
+            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+          ) : (
+            <ReactSortable
+              animation={150}
+              list={group.children}
+              setList={list => setList(index, list)}
+              group={{ name: groupName, pull: "clone", put: false }}
+              clone={item => ({ ...item, [rowKey]: uuid() })}
+              sort={false}
+            >
+              {group.children.map(item => (
+                <CellField title={item.title} type={item.type} icon={item.icon} onClick={() => onItem({ ...item, [rowKey]: uuid() })} key={item[rowKey]} />
+              ))}
+            </ReactSortable>
+          )}
         </Collapse.Panel>
       ))}
     </Collapse>
@@ -51,13 +51,14 @@ function TemplatePanel() {
   return <div>模板</div>;
 }
 
-function TabFields({ component }) {
+function TabFields({ component, onAdd }) {
   return (
     <Tabs
       items={[
         ...(!component ? [] : [{ label: <IconTip showTitle title="组件" icon="icon-component" />, key: "form", children: component }]),
         // ...(!template ? [] : [{ label: <IconTip showTitle title="模板" icon="icon-packaging" />, key: "component", children: "template" }]),
       ]}
+      tabBarExtraContent={<Button type="link" size="small" icon={<Icon type="icon-add" />} onClick={onAdd} />}
     />
   );
 }
